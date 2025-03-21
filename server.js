72bf73f1d8e4382b900dbc3beac4d175c73eb9ab
rubb3rDucc <thomalin@oregonstate.edu>
2025-03-21 17:43:53 -0400

    added server and db code

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+const dbConnection_1 = require("./dbConnection");
 const express = require("express");
 const http = require("http");
+const morgan = require("morgan");
+dotenv_1.default.config();
 const app = express();
 const router = express.Router();
 const server = http.createServer(app);
-const port = 3000;
+const allowedOrigins = ["http://localhost:3000"];
+const port = process.env.PORT;
+app.use(morgan("tiny"));
+app.use("/api/v1", router);
+app.use(express.static(__dirname + '/public'));
 router.use((req, res, next) => {
-    res.header("Access-Control-Allow-Methods", "GET,POST");
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+        res.header("Access-Control-Allow-Origin", origin);
+    }
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 router.get("/", (req, res) => {
@@ -20,9 +38,33 @@ router.get("/health", (req, res) => {
     };
     res.status(200).send(data);
 });
-router.put('/incrementcount', (req, res) => {
+router.get("/count", (req, res) => {
+    dbConnection_1.client.query(`SELECT * FROM button_metrics;`, (err, results) => {
+        if (err)
+            throw err;
+        res.send(results.rows);
+    });
 });
-app.use('/api/v1', router);
+router.post("/increment", (req, res) => {
+    dbConnection_1.client.query(`UPDATE button_metrics 
+                SET times_pressed = times_pressed + 1, 
+                    last_pressed = now();`, (err, results) => {
+        if (err)
+            throw err;
+        res.send(results.rows);
+        console.log("incremented button count");
+    });
+});
+router.post("/decrement", (req, res) => {
+    dbConnection_1.client.query(`UPDATE button_metrics 
+                SET times_pressed = times_pressed - 1, 
+                    last_pressed = now();`, (err, results) => {
+        if (err)
+            throw err;
+        res.send(results.rows);
+        console.log("decremented button count");
+    });
+});
 server.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
--- server.ts
+++ server.ts
@@ -1,14 +1,37 @@
+import { Query } from "./node_modules/@types/pg/index.d";
+import dotenv from "dotenv";
+import { client } from "./dbConnection";
+
 const express = require("express");
 const http = require("http");
+const morgan = require("morgan");
+
+dotenv.config();
 
 const app = express();
 const router = express.Router();
 const server = http.createServer(app);
+const allowedOrigins = ["http://localhost:3000"];
 
-const port = 3000;
+const port = process.env.PORT;
 
-router.use((req:any, res:any, next:any) => {
-  res.header("Access-Control-Allow-Methods", "GET,POST");
+app.use(morgan("tiny"));
+app.use("/api/v1", router);
+app.use(express.static(__dirname + '/public'));
+
+router.use((req: any, res: any, next: any) => {
+  const origin = req.headers.origin;
+
+  if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
@@ -26,12 +49,34 @@ router.get("/health", (req: any, res: any) => {
   res.status(200).send(data);
 });
 
-router.put('/incrementcount', (req: any, res: any) => {
+router.get("/count", (req: any, res: any) => {
+  client.query(`SELECT * FROM button_metrics;`, (err: any, results: any) => {
+    if (err) throw err;
+    res.send(results.rows);
+  });
+});
 
-})
+router.post("/increment", (req: any, res: any) => {
+  client.query(`UPDATE button_metrics 
+                SET times_pressed = times_pressed + 1, 
+                    last_pressed = now();`,
+      (err:any, results:any) => {
+      if (err) throw err;
+      res.send(results.rows);
+      console.log("incremented button count");
+    });
+});
 
-app.use('/api/v1', router);
-
+router.post("/decrement", (req: any, res: any) => {
+  client.query(`UPDATE button_metrics 
+                SET times_pressed = times_pressed - 1, 
+                    last_pressed = now();`,
+      (err:any, results:any) => {
+      if (err) throw err;
+      res.send(results.rows);
+      console.log("decremented button count");
+    });
+});
 
 server.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
