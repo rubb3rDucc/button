64dec6e2340748781bc4b12cda9c86f0fd70837d
rubb3rDucc <thomalin@oregonstate.edu>
2025-03-25 02:22:46 -0400

    move these

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -1,16 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.pool = void 0;
 const { Pool } = require("pg");
 require("dotenv").config();
-const client = new Pool({
+const pool = new Pool({
     host: process.env.HOST,
     user: process.env.USER,
     database: process.env.DB_NAME,
     port: process.env.DB_PORT,
 });
-client.connect((err) => {
-    if (err)
-        throw err;
-    console.log("Connected to Database.");
-});
-exports.default = client;
+exports.pool = pool;
--- src/db/dbConnection.ts
+++ src/db/dbConnection.ts
@@ -1,16 +1,11 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const client = new Pool({
+const pool = new Pool({
   host: process.env.HOST,
   user: process.env.USER,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
 });
 
-client.connect((err:any) => {
-  if (err) throw err;
-  console.log("Connected to Database.");
-});
-
-export default client;
+export { pool };
