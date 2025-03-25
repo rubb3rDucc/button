138e3052c93f9ff42e7cb4c0397f07f34a9ed2aa
rubb3rDucc <thomalin@oregonstate.edu>
2025-03-25 17:08:36 -0400

    moved server listen again

--- src/server.js
+++ src/server.js
@@ -14,21 +14,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 const express_1 = __importDefault(require("express"));
-// import morgan from "morgan";
+const morgan_1 = __importDefault(require("morgan"));
 const http_1 = __importDefault(require("http"));
 const routes_1 = require("./controllers/routes");
 const dbConnection_1 = require("./db/dbConnection");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const server = http_1.default.createServer(app);
-// const port = process.env.PORT;
-// app.use(morgan("short"));
+const port = process.env.PORT;
+app.use((0, morgan_1.default)("short"));
 app.use("/api/v1", routes_1.router);
 app.use(express_1.default.static(__dirname + '/public'));
-// pool.connect((err: any) => {
-//   if (err) throw err;  
-//   server.listen(port);
-// });
+dbConnection_1.pool.connect((err) => {
+    if (err)
+        throw err;
+});
+server.listen(port);
 function closeServer() {
     return __awaiter(this, void 0, void 0, function* () {
         yield server.close((err) => {
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
-// import morgan from "morgan";
+import morgan from "morgan";
 import http from "http";
 import { router } from "./controllers/routes"
 import {pool} from "./db/dbConnection"
@@ -17,8 +17,9 @@ app.use(express.static(__dirname + '/public'));
 
 pool.connect((err: any) => {
   if (err) throw err;  
-  server.listen(port);
 });
+
+server.listen(port);
 
 async function closeServer() {
   await server.close((err)=>{
