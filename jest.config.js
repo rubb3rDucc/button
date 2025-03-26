af935e125418c9eb7225289891b8bce33e92e040
rubb3rDucc <thomalin@oregonstate.edu>
2025-03-25 20:47:00 -0400

    add more

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
+  testMatch : [
+    '<rootDir>/dist/**/__tests__/**/*.{js,jsx,ts,tsx}',
+    '<rootDir>/dist/**/*.{spec,test}.{js,jsx,ts,tsx}',
+  ],
   roots: ["<rootDir>/dist/tests/"],
 }
