ddc371b135f7f31cc6de99a94dd2cbdd612f609f
rubb3rDucc <thomalin@oregonstate.edu>
2025-03-25 20:55:51 -0400

    remove everything

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,3 @@
 module.exports = {
-  testMatch : [
-    '<rootDir>/dist/**/__tests__/**/*.{js,jsx,ts,tsx}',
-    '<rootDir>/dist/**/*.{spec,test}.{js,jsx,ts,tsx}',
-  ],
-  roots: ["<rootDir>/dist/tests/"],
+  roots: ["<rootDir>/dist"],
 }
