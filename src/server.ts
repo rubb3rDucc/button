4f474235b6b0eb4269657b4734d82115a684b0fd
rubb3rDucc <thomalin@oregonstate.edu>
2025-04-12 16:53:59 -0400

    added ws, add controller, rm unused close function

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,11 @@ import express from "express";
 import morgan from "morgan";
 import http from "http";
 import { router } from "./controllers/routes.js";
-import {pool} from "./db/dbConnection.js";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { pool } from "./db/dbConnection.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 import cors from "cors";
+import { setupWebSocketServer } from "./controllers/websocketController.js";
 
 dotenv.config();
 
@@ -15,26 +16,19 @@ const __dirname = dirname(__filename);
 const app = express();
 const server = http.createServer(app);
 const port = process.env.PORT;
+const wss = setupWebSocketServer(server);
 
 app.use(morgan("short"));
 app.use("/api/v1", router);
 app.use(cors());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
 pool.connect((err: any) => {
-  if (err) throw err;  
+  if (err) throw err;
 });
 
-server.listen(port);
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
-async function closeServer() {
-  await server.close((err)=>{
-      pool.end();
-  });
-}
-
-export {
-  app,
-  server,
-  closeServer
-}
+export { app, server, wss };
